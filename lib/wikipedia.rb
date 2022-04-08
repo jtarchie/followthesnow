c8fb8d072403cc479056c60117291bb45371dd57
JT Archie <jtarchie@gmail.com>
2022-04-08 08:17:22 -0600

    add Vermont

--- lib/wikipedia.rb
+++ lib/wikipedia.rb
@@ -83,8 +83,9 @@ if __FILE__ == $PROGRAM_NAME
     'https://en.wikipedia.org/wiki/Category:Ski_areas_and_resorts_in_New_Mexico',
     'https://en.wikipedia.org/wiki/Category:Ski_areas_and_resorts_in_Oregon',
     'https://en.wikipedia.org/wiki/Category:Ski_areas_and_resorts_in_Utah',
+    'https://en.wikipedia.org/wiki/Category:Ski_areas_and_resorts_in_Vermont',
     'https://en.wikipedia.org/wiki/Category:Ski_areas_and_resorts_in_Washington_(state)',
-    'https://en.wikipedia.org/wiki/Category:Ski_areas_and_resorts_in_Wyoming'
+    'https://en.wikipedia.org/wiki/Category:Ski_areas_and_resorts_in_Wyoming',
   ]
 
   puts 'name,lat,lng,city,state,url,forecast_url'
